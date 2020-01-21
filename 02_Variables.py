adc5c418b52ea4bd8d1e14fbe97ecd6a6492afb2
Valdaz007 <59840456+Valdaz007@users.noreply.github.com>
2020-01-22 02:10:45 +1000

    Python Variables and Data Types

---
+++ 02_Variables.py
@@ -0,0 +1,24 @@
+# What is a Variable?
+# - A Variable is a Value Place Holder.
+#   Variables are References to the
+#   Computer's Memory that stores the 
+#   Values Assign to Variable when Created.
+
+# Data Types
+# - Numbers
+# - String
+
+# Advance Data Types For Data Structure
+# - List
+# - Tuple
+# - Dictionary
+
+# Creating a Variable & Assigning a Value
+age = 22        # A Integer Variable
+weight = 100.6  # A Floating Point Variable
+name = "Victor" # A String Varible
+
+
+print(name)
+print(age)
+print(weight)
---
+++ 03_Variables.py
@@ -0,0 +1,14 @@
+# Lists
+alist = []                                  # An Empty List
+numlist = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]    # A List Containing Numbers
+strlist = ['Hello', 'World']                # A List Containing Texts / Strings
+mixlist = ['Victor', 25, 'Male', 20161232]  # A List Containing Mix Data Types
+
+print(alist)
+print(numlist)
+print(strlist)
+print(mixlist)
+
+# Accessing List Index
+print(numlist[1])
+print(strlist[1])
