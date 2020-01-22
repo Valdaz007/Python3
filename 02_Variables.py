7bf172fda972e4fe363eefe0ef1c127061b0b79b
Valdaz007 <59840456+Valdaz007@users.noreply.github.com>
2020-01-23 03:14:20 +1000

    Update 02_Variables.py
    List Variable

--- 02_Variables.py
+++ 02_Variables.py
@@ -1,4 +1,8 @@
 # Lists
+# A List contain items seperated by Commas (,) and are enclosed within Brackets ([]).
+# List is similar to Arrays in C and C++ but can contain different data types.
+
+# Creating a List:
 alist = []                                  # An Empty List
 numlist = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]    # A List Containing Numbers
 strlist = ['Hello', 'World']                # A List Containing Texts / Strings
@@ -9,6 +13,34 @@ print(numlist)
 print(strlist)
 print(mixlist)
 
+input()
+
 # Accessing List Index
 print(numlist[1])
 print(strlist[1])
+
+input()
+
+# Adding Items to List using In-Built List Method 'append()'
+alist.append('First')
+numlist.append(10)
+strlist.append('Victor')
+mixlist.append('23-10-94')
+
+print(alist)
+print(numlist)
+print(strlist)
+print(mixlist)
+
+input()
+
+#Removing Items From a List using In-Built List Method 'pop()'
+alist.pop()
+numlist.pop(0)
+strlist.pop(1)
+mixlist.pop()
+
+print(alist)
+print(numlist)
+print(strlist)
+print(mixlist)
