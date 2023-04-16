4e90070964eb1214a8864526001e33feb4f685e3
Victor Volsavai <59840456+Valdaz007@users.noreply.github.com>
2023-04-17 09:37:43 +1000

    Update 12_Dice_Roll.py

--- 12_Dice_Roll.py
+++ 12_Dice_Roll.py
@@ -1,6 +1,6 @@
 import random  # Module for Generating a Random Number
 
-def roll_Dice() -> None:
+def roll_Dice() -> int:
 	return random.randrange(1, 7, 1) # Generate a Random Number between 1 & 6
 
 if __name__ == "__main__":
